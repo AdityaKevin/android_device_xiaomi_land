91d010f75ae030652b07bf38d60ab72bf77dd52b
erfanoabdi <erfangplus@gmail.com>
2019-09-05 23:18:10 +0200

    land: Drop unnecessary BootJars from device.mk
    Change-Id: I3b0eab2cf49b9ebc278f1e93c95086cc9880e5b6
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -199,9 +199,6 @@ PRODUCT_PACKAGES += \
     libgnsspps \
     libcurl
 
-PRODUCT_BOOT_JARS += \
-    com.qti.location.sdk
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
@@ -437,9 +434,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl
-
- PRODUCT_BOOT_JARS += \
-    WfdCommon
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
